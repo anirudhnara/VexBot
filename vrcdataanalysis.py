414be5a5f089f1f0ca0506fdb2a19265502c883d
Gamoxu <79703488+Gamoxu@users.noreply.github.com>
2023-12-05 19:27:30 -0500

    Update vrcdataanalysis.py

--- vrcdataanalysis.py
+++ vrcdataanalysis.py
@@ -7,16 +7,16 @@ def prediction(red1, red2, blue1, blue2):
     r = requests.get(request_url)
     return r.json()
 
-def team_trueskill_rank(team):
-    request_url = "https://vrc-data-analysis.com/v1/team/" + team
-    r = requests.get(request_url)
-    data = r.json()
+#def team_trueskill_rank(team):
+    #request_url = "https://vrc-data-analysis.com/v1/team/" + team
+    #r = requests.get(request_url)
+    #data = r.json()
 
-    return data['trueskill_ranking']
+    #return data['trueskill_ranking']
 
-def team_trueskill_value(team):
-    request_url = "https://vrc-data-analysis.com/v1/team/" + team
-    r = requests.get(request_url)
-    data = r.json()
+#def team_trueskill_value(team):
+    #request_url = "https://vrc-data-analysis.com/v1/team/" + team
+    #r = requests.get(request_url)
+    #data = r.json()
 
-    return data['trueskill']
+    #return data['trueskill']
